ac4f25b39968ece1c944841cda8e451209674b3f
faiqueelliot <49121486+faiqueelliot@users.noreply.github.com>
2022-02-22 22:05:05 +0530

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 session_start(); 
 include "db_conn.php";
 
-if (isset($_POST['uname']) && isset($_POST['password'])) {
+if (isset($_GET['uname']) && isset($_GET['password'])) {
 
 	function validate($data){
        $data = trim($data);
